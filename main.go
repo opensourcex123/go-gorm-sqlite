92ed19f9c9db1e158347aa55dcf8c6bf211511d7
nfx_git <13561592791@163.com>
2021-12-17 15:13:37 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	Title string
+	Code string
+	Price uint
+}
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&Product{})
+
+	// 插入内容
+	db.Create(&Product{Title: "新款手机", Code: "D42", Price: 1000})
+	db.Create(&Product{Title: "新款电脑", Code: "D43", Price: 3500})
+
+	// 读取内容
+	var product Product
+	db.First(&product, 1)
+	db.First(&product, "code = ?", "D42")
+
+	// 更新操作
+	db.Model(&product).Update("Price", 2000)
+
+	// 更新多个字段
+	db.Model(&product).Updates(Product{Price: 2000, Code: "F42"}) // non-zero fields
+	db.Model(&product).Updates(map[string]interface{}{"Price": 2000, "Code": "F42"})
+
+	// 删除操作
+	db.Delete(&product, 1)
+}
